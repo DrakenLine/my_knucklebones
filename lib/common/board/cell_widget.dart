64325d402e879cf9831f1dd582bc8d65a2ad6c61
Alexandra Farina <alexandra.farina@ynov.com>
2025-02-18 18:45:38 +1100

    feat: init cell widget

---
+++ lib/common/board/cell_widget.dart
@@ -0,0 +1,40 @@
+import 'package:dotted_border/dotted_border.dart';
+import 'package:flutter/material.dart';
+import 'package:my_knucklebones/theme/app_theme_data.dart';
+
+class CellWidget extends StatelessWidget {
+  final bool isSelected;
+  const CellWidget({
+    super.key,
+    this.isSelected = false,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    const AppThemeData theme = AppThemeData();
+
+    return Container(
+      margin: EdgeInsets.all(10),
+      child: Stack(
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(5),
+              color: isSelected ? theme.colors.earthYellow : Colors.transparent,
+            ),
+          ),
+          DottedBorder(
+            strokeWidth: isSelected ? 2 : 1,
+            color: isSelected
+                ? theme.colors.butterScotch
+                : theme.colors.blackOlive,
+            borderType: BorderType.RRect,
+            radius: Radius.circular(5),
+            dashPattern: <double>[6, 6],
+            child: Container(),
+          ),
+        ],
+      ),
+    );
+  }
+}
